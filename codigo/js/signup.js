fe82d0b408813660a64d50bd2ca88a2b13e469fb
miguelromero1 <miguelromeroromero@cedoc.edu.co>
2023-05-09 00:15:00 -0500

    catualizacion

--- codigo/js/signup.js
+++ codigo/js/signup.js
@@ -1,23 +1,34 @@
 const signupform = document.querySelector("#signupform");
+
+// Manejador de eventos para el envío del formulario
 signupform.addEventListener("submit", (event) => {
     event.preventDefault();
-    const name= document.querySelector('#name') 
-    const emailInput = document.querySelector('#email') 
-    const password= document.querySelector('#password') 
 
+    // Obtener valores de los campos del formulario
+    const nameInput = document.querySelector('#name') 
+    const emailInput = document.querySelector('#email') 
+    const passwordInput = document.querySelector('#password') 
+
+    // Obtener usuarios del almacenamiento local o establecer un array vacío si no hay usuarios
     const users = JSON.parse(localStorage.getItem('users')) || [];
 
+    // Verificar si el correo electrónico ya está registrado
     const searchEmail = emailInput.value;
     const isUserRegistered = users.find(user => user.email === searchEmail);
 
+    // Si el correo electrónico ya está registrado, mostrar una alerta
     if (isUserRegistered) {
         alert('El usuario ya está registrado.');
     } else {
-        const nameValue = name.value;
-        const passwordValue = password.value;
+        // Si el correo electrónico no está registrado, agregar el nuevo usuario al array y actualizar el almacenamiento local
+        const nameValue = nameInput.value;
+        const passwordValue = passwordInput.value;
 
         users.push({name: nameValue, email: searchEmail, password: passwordValue});
         localStorage.setItem('users', JSON.stringify(users));
         alert('Registro exitoso.');
-    }window.location.href= 'login.html'
+    }
+
+    // Redirigir a la página de inicio de sesión después de registrar un nuevo usuario
+    window.location.href= 'login.html'
 })
